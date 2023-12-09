ad4a04916353921acb8f370367d730da9d94c5d5
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2023-12-09 14:11:13 -0800

    Styling for larger devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useMediaQuery } from "react-responsive";
 import "./sass/App.scss";
 import mobileBg from "./assets/background-pattern-mobile.svg";
+import desktopBg from "./assets/background-pattern-desktop.svg";
 import Card from "./components/Card";
 
 function App() {
+  const isGreaterThan600px = useMediaQuery({ query: "(min-width:600px)" });
+
   return (
     <div className="app">
       <section className="bg">
-        <img src={mobileBg} alt="mobileBg" />
+        <img src={isGreaterThan600px ? desktopBg : mobileBg} alt="background" />
       </section>
       <Card />
     </div>
